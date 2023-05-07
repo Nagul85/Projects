a02c16778bfc04336c2ace5a75eb015696e4cf54
Nagul85 <95899167+Nagul85@users.noreply.github.com>
2023-05-07 22:47:19 +0530

    mywork
    Project

---
+++ Html,Css/7/index.js
@@ -0,0 +1,15 @@
+var menus = document.getElementById("menu-items");
+
+      menus.style.maxHeight="0px";
+      
+function menutoggle(){
+
+ if(menus.style.maxHeight="0px"){
+    menus.style.maxHeight="200px";
+ }
+ else
+ {
+    menus.style.maxHeight="0px";
+ }
+
+}
